f2ff085e1869e13a0eafa96d34cf30341634465b
Sanaullah Malik <129273806+maliksanaulah@users.noreply.github.com>
2024-06-07 23:46:15 +0500

    Add files via upload

---
+++ View created.sql
@@ -0,0 +1,9 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[Total Amount of Vaccination]
+  FROM [Covid 19 DB].[dbo].[PercentPopulationVaccinated]
+
+  select * from PercentPopulationVaccinated
